a1d6d4e97f4fd452785b0c119959f977fa450fef
acode-writer <souhaibou.toure122@gmail.com>
2022-04-21 07:24:47 +0000

    track express in package.json.

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,17 @@
-import app from "./app.js";
 import http from "http";
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
 
+import rssfeedRoute from "./src/routes/rssfeed.route.js"
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true}));
+
+app.use("/", rssfeedRoute);
 
 app.set("port", process.env.PORT);
 const server = http.createServer(app);
